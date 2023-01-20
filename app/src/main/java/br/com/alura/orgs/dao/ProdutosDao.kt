2ad3e54692b092495b5d22edc37bbf794a69bc00
Thamires Santos <thamires.santos@picpay.com>
2023-01-20 16:22:03 -0300

    Initial Commit

---
+++ app/src/main/java/br/com/alura/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,24 @@
+package br.com.alura.orgs.dao
+
+import br.com.alura.orgs.model.Produto
+import java.math.BigDecimal
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto){
+        produtos.add(produto)
+    }
+
+    fun buscaTodos() : List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>(
+            Produto(nome = "Salada de Frutas",
+                descricao = "Laranja, maçās e uva",
+                valor = BigDecimal("19.83")
+        ))
+    }
+
+}
